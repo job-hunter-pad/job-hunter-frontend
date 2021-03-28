10f196ebd1d781f7a1b5d7e44684f65a359fdc40
Tania Topciov <taniatopciov@yahoo.com>
2021-03-28 12:35:33 +0300

    Created Authentication Service

---
+++ src/app/authentication/authentication.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private loginUrl = "/api/auth/login";
+  private registerUrl = "/api/auth/register";
+
+  constructor(private http: HttpClient) { }
+
+  login(email, password) {
+    return this.http.post(
+      this.loginUrl,
+      JSON.stringify({email: email, password: password})
+    );
+  }
+
+  register(name, email, password, userType) {
+    return this.http.post(
+      this.registerUrl, 
+      JSON.stringify({name: name, email: email, password: password, userType: userType})
+    );
+  }
+}
