f216bd6cc3aac3e28b72672ff858d3921264208b
flavia.velcsov <v_flavia99@yahoo.com>
2021-04-29 12:52:58 +0300

    Job Card Design adjusted

--- src/app/mock-jobs.ts
+++ src/app/mock-jobs.ts
@@ -1,16 +1,16 @@
 import {Job} from './job'
 
 export const JOBS: Job[] = [
-    { job_name : "Angajez ajutor bucatar",job_salary:"12$" ,employer_name:"Adrian Verde", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Caut mecanic",job_salary:"10$" , employer_name:"Vasile Petrovan", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Site Web (salariu atractiv)",job_salary:"10$" ,employer_name:"MediaX", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Babysiter", job_salary:"10$" ,employer_name:"Cosmina Lof", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Angajez ajutor bucatar",job_salary:"10$" ,employer_name:"Adrian Verde", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Caut mecanic",job_salary:"10$" , employer_name:"Vasile Petrovan", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Site Web (salariu atractiv)",job_salary:"10$" ,employer_name:"MediaX", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Babysiter", job_salary:"10$" ,employer_name:"Cosmina Lof", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Angajez ajutor bucatar",job_salary:"10$" ,employer_name:"Adrian Verde", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Caut mecanic",job_salary:"10$" , employer_name:"Vasile Petrovan", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Site Web (salariu atractiv)",job_salary:"10$" ,employer_name:"MediaX", date: new Date('2021-04-18').toLocaleDateString()},
-    { job_name : "Babysiter", job_salary:"10$" ,employer_name:"Cosmina Lof", date: new Date('2021-04-18').toLocaleDateString()}
+    { job_name : "Angajez ajutor bucatar",job_salary:"12" ,employer_name:"Adrian Verde", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Caut mecanic",job_salary:"10" , employer_name:"Vasile Petrovan", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Site Web (salariu atractiv)",job_salary:"10" ,employer_name:"MediaX", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Babysiter", job_salary:"10" ,employer_name:"Cosmina Lof", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Angajez ajutor bucatar",job_salary:"10" ,employer_name:"Adrian Verde", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Caut mecanic",job_salary:"10" , employer_name:"Vasile Petrovan", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Site Web (salariu atractiv)",job_salary:"10" ,employer_name:"MediaX", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Babysiter", job_salary:"10" ,employer_name:"Cosmina Lof", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Angajez ajutor bucatar",job_salary:"10" ,employer_name:"Adrian Verde", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Caut mecanic",job_salary:"10" , employer_name:"Vasile Petrovan", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Site Web (salariu atractiv)",job_salary:"10" ,employer_name:"MediaX", date: new Date('2021-04-18').toLocaleDateString()},
+    { job_name : "Babysiter", job_salary:"10" ,employer_name:"Cosmina Lof", date: new Date('2021-04-18').toLocaleDateString()}
 ];
