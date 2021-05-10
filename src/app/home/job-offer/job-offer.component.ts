0bdd6acae78a58a1d72d990155c03834e529f611
Tania Topciov <taniatopciov@yahoo.com>
2021-05-10 20:39:32 +0300

    Modified Job Offer Component

--- src/app/home/job-offer/job-offer.component.ts
+++ src/app/home/job-offer/job-offer.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import {Job} from '../../job'
-import {RenegotiatonComponent} from './renegotiaton/renegotiaton.component'
+import { JobOffer } from 'src/app/jobs/jobOffer';
+import { Job } from '../../job'
+import { RenegotiatonComponent } from './renegotiaton/renegotiaton.component'
 
 @Component({
   selector: 'app-job-offer',
@@ -14,14 +15,14 @@ export class JobOfferComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<JobOfferComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Job,
-    public dialog: MatDialog) {}
+    @Inject(MAT_DIALOG_DATA) public data: JobOffer,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
   openRenegotiation(): void {
-    this.dialog.open(RenegotiatonComponent,{
+    this.dialog.open(RenegotiatonComponent, {
       width: '400px',
       height: '400px',
       data: null
