0bbe8d09dc1fa78bd0c6ab465073b1b6eba35063
Tania Topciov <taniatopciov@yahoo.com>
2021-05-11 20:27:34 +0300

    Layout Improvment for Offers List Element

--- src/app/profiles/employer-profile/offers-list-element/offers-list-element.component.ts
+++ src/app/profiles/employer-profile/offers-list-element/offers-list-element.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { JobsService } from 'src/app/jobs/jobs.service';
+import { EventEmitter } from '@angular/core';
+import { JobApplication } from 'src/app/jobs/jobApplication';
+import { JobOffer } from 'src/app/jobs/jobOffer';
 
 @Component({
   selector: 'app-offers-list-element',
@@ -7,9 +11,37 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class OffersListElementComponent implements OnInit {
 
-  @Input() job;
+  private _job: JobOffer;
 
-  constructor() { }
+  isApplicationComplete = false;
+
+  @Input() set job(value: JobOffer) {
+
+    this._job = value;
+    for (let i = 0; i < this._job.applications.length; i++) {
+      const element = this._job.applications[i];
+      if (element.status == 'ACCEPTED' || element.status == 'COMPLETED') {
+        this.acceptedApplication = element;
+        if (element.status == 'COMPLETED') {
+          this.isApplicationComplete = true;
+        }
+        break;
+      }
+    }
+  }
+
+  get job(): JobOffer {
+
+    return this._job;
+
+  }
+
+  @Output() applicationHandleEvent: EventEmitter<any> = new EventEmitter();
+
+  value: number;
+  acceptedApplication: JobApplication;
+
+  constructor(private jobService: JobsService) { }
 
   ngOnInit(): void {
   }
@@ -19,5 +51,26 @@ export class OffersListElementComponent implements OnInit {
       return true;
     }
     return false;
+  }
+
+  isStatusInProgress() {
+    if (this.job.status == 'IN_PROGRESS') {
+      return true;
+    }
+    return false;
+  }
+
+  onAcceptApplicationButtonClick(applicationId) {
+    this.jobService.acceptApplication(this.job.id, applicationId).subscribe(application => {
+      this.applicationHandleEvent.emit()
+    });
+  }
+
+  onRejectApplicationButtonClick(applicationId) {
+    this.jobService.rejectApplication(this.job.id, applicationId).subscribe(application => { this.applicationHandleEvent.emit() });
+  }
+
+  completeJobOffer() {
+
   }
 }
